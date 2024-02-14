729168340d1a01c560c914f500663aaaeaf5904e
ngodingbentar <dewaqintoro@gmail.com>
2024-02-15 00:49:42 +0700

    updated: route

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ db.once('open', () => console.log('Connected to MongoDB'));
 
 app.use(cors());
 app.use(express.json());
-app.use(UserRoute)
+// app.use(UserRoute)
+app.use('/api/v1', UserRoute)
+
+app.get('/', (req, res) => res.send('Home Page Route'));
 
 app.listen(5007, () => {
   console.log('Listening on port 5007');
